c2523688831568f279a6ae9c2576b106b6cd3b68
shosato <sho.sato@gearset.com>
2021-06-09 11:11:57 +0100

    Add models

---
+++ ApiVersioningPoc/ApiVersioningPoc/Models/Version.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiVersioningPoc.Models
+{
+    public class Version
+    {
+        public string Name { get; set; }
+
+        public Version(string name)
+        {
+            Name = name;
+        }
+    }
+}
